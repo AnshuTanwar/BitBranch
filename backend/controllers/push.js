cf49b2704d381c2491695166e8f942b6acb1997f
Aanshu Tanwar <aanshutanwar@Aanshus-MacBook-Air.local>
2025-07-14 19:02:03 +0530

    Set up basic terminal commands logic

---
+++ backend/controllers/push.js
@@ -0,0 +1,5 @@
+async function pushRepo() {
+    console.log("add command called");
+}
+
+module.exports = { pushRepo };
---
+++ backend/server.js
@@ -0,0 +1,53 @@
+const yargs = require('yargs');
+const { hideBin } = require("yargs/helpers");
+const { initRepo } = require('./controllers/init.js');
+const { addRepo } = require('./controllers/add.js');
+const { commitRepo } = require('./controllers/commit.js');
+const { pullRepo } = require('./controllers/pull.js');
+const { pushRepo } = require('./controllers/push.js');
+const { revertRepo } = require('./controllers/revert.js');
+
+yargs(hideBin(process.argv))
+    .command(
+        'init',
+        "Initialise the new repository",
+        {},
+        initRepo
+    )
+    .command(
+        'add <file>',
+        "Add a file to the repository",
+        (yargs) => {
+            yargs.positional("file", {
+                description: "File to add to the staging area",
+                type: "string",
+            });
+        },
+        addRepo
+    )
+    .command(
+        'commit <message>',
+        "Commit the staged files",
+        (yargs) => {
+            yargs.positional("message", {
+                description: "Commit message",
+                type: "string",
+            });
+        },
+        commitRepo
+    )
+    .command("push", "Push commits to S3", {}, pushRepo)
+    .command("pull", "Pull commits from S3", {}, pullRepo)
+    .command(
+        'revert <commitID>',
+        "Revert a specific commit",
+        (yargs) => {
+            yargs.positional("commitID", {
+                description: "Commit ID to revertt to",
+                type: "string",
+            });
+        },
+        revertRepo
+    )
+    .demandCommand(1, "You need at least one command")
+    .help().argv;
