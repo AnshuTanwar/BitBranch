b8bb33bb988dbe795ae49e2dc7528084e6a9e4d3
Aanshu Tanwar <aanshutanwar07@gmail.com>
2025-08-02 00:00:18 +0530

    setting up signup route

---
+++ backend/controllers/issueController.js
@@ -0,0 +1,27 @@
+const createIssue = (req, res) => {
+    res.send("issue created");
+};
+
+const updateIssueById = (req, res) => {
+    res.send("issue created");
+};
+
+const deleteIssueById = (req, res) => {
+    res.send("issue created");
+};
+
+const getAllIssues = (req, res) => {
+    res.send("issue created");
+};
+
+const getIssueById = (req, res) => {
+    res.send("issue created");
+};
+
+module.exports = {
+    createIssue,
+    updateIssueById,
+    deleteIssueById,
+    getAllIssues,
+    getIssueById,
+};
